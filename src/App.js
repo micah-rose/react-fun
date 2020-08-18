59659e6c3bfdd2cd7d41e7a356e30564f755b128
Micah Lund <micah.r.lund@gmail.com>
2020-08-18 15:10:38 -0600

    Made some tweaks so that page will render.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Tetris from './components/Tetris';
 import './App.css';
 
-const App = () => {
+const App = () => (
 
   <div className="App">
     <Tetris />
   </div>
 
-}    
+)    
 
 export default App;
