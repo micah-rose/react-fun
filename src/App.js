551202c5a9bb58be4a33b0eb6acd52e2c360fa52
Micah Lund <micah.r.lund@gmail.com>
2020-08-18 14:20:20 -0600

    Set up foundation for App.js and started setting up components folder with a Tetris component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Tetris from './components/Tetris';
 import './App.css';
 
-function App() {
+const App = () => {
 
-  
+  <div className="App">
+    <Tetris />
+  </div>
+
 }    
 
 export default App;
