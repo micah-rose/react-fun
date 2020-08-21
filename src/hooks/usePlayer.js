2e4d8645caa623caabeb8f41c13d79da2407808f
Micah Lund <micah.r.lund@gmail.com>
2020-08-21 10:53:39 -0600

    Updated usePlayer to match tutorial and realized I was missing parenthesis in the rotate function when calling rotatedTetro.reverse.

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,33 +1,35 @@
 import {useState, useCallback} from 'react';
-import {TETROMINOS, randomTetro} from '../tetrominos';
+
+import {TETROMINOS, randomTetromino} from '../tetrominos';
 import { STAGE_WIDTH, checkCollision } from '../gameHelpers';
 
 export const usePlayer = () => {
     const [player, setPlayer] = useState({
         pos: {x: 0, y: 0},
-        tetro: TETROMINOS[0].shape,
+        tetromino: TETROMINOS[0].shape,
         collided: false 
     });
 
     const rotate = (matrix, dir) => {
         const rotatedTetro = matrix.map((_, index) => 
-            matrix.map(col => col[index]))
+            matrix.map(col => col[index]),
+        );
 
         if (dir > 0) return rotatedTetro.map(row => row.reverse());
-        return rotatedTetro.reverse;
+        return rotatedTetro.reverse();
     }
 
     const playerRotate = (stage, dir) => {
         const clonedPlayer = JSON.parse(JSON.stringify(player));
-        clonedPlayer.tetro = rotate(clonedPlayer.tetro, dir);
+        clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
 
         const pos = clonedPlayer.pos.x;
         let offset = 1;
-        while (checkCollision(clonedPlayer, stage, {x: 0, y: 0})){
+        while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })){
             clonedPlayer.pos.x += offset;
             offset = -(offset + (offset > 0 ? 1 : -1));
-            if (offset > clonedPlayer.tetro[0].length){
-                rotate(clonedPlayer.tetro, -dir)
+            if (offset > clonedPlayer.tetromino[0].length){
+                rotate(clonedPlayer.tetromino, -dir);
                 clonedPlayer.pos.x = pos;
                 return;
             }
@@ -36,18 +38,18 @@ export const usePlayer = () => {
         setPlayer(clonedPlayer);
     }
 
-    const updatePlayerPos = ({x, y, collided}) => {
+    const updatePlayerPos = ({ x, y, collided }) => {
         setPlayer(prev => ({
             ...prev,
-            pos: {x: (prev.pos.x += x), y: (prev.pos.y += y)},
+            pos: { x: (prev.pos.x += x), y: (prev.pos.y += y) },
             collided
         }))
     }
 
     const resetPlayer = useCallback(() => {
         setPlayer({
-            pos: {x: STAGE_WIDTH / 2 - 2, y: 0},
-            tetro: randomTetro().shape,
+            pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+            tetromino: randomTetromino().shape,
             collided: false 
         })
     }, [])
